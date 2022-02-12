b8494988cbd1721a4dbb129b8e47e2df6cc4647e
Rogger Vásquez <rogger.vasquez@gmail.com>
2022-02-11 19:58:51 -0600

    test change

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,6 +1,6 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-  greeting = 'Universe';
+  greeting = 'Universe ss';
   changeHandler(event) {
     this.greeting = event.target.value;
   }
